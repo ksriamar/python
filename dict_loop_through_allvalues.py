5afbcec6a7802a70952727981dc0e67f6b235afa
sriamar <58838786+ksriamar@users.noreply.github.com>
2021-07-15 06:24:03 +0530

    Add files via upload

---
+++ dict_loop_through_allvalues.py
@@ -0,0 +1,4 @@
+fav_lang = {'amar': 'python', 'nitesh': 'R', 'abhi': 'ruby', 'mohit': 'python',}
+print("The languages that are mentioned:")
+for language in fav_lang.values():
+    print(language.title())
